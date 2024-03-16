22c9e201a4d1bf6b0fa587fee57b75e8155fe340
Gelila Solomon <83179822+GELILASOLOMON@users.noreply.github.com>
2024-03-16 11:21:01 +0300

    Teachers folder

---
+++ client/pages/teachers/notification/index.js
@@ -0,0 +1,10 @@
+import RootLayout from '@/layouts/RootLayout'
+const Notification = () => {
+    return ( 
+        <RootLayout>
+            <div> Notification </div>
+        </RootLayout>
+     );
+}
+ 
+export default Notification;
